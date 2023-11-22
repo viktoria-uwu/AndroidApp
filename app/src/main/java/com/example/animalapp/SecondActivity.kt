d1f75ba8d66a65f9caa3b186e2e9d10cd1a474c2
viktoria <viktoriashvets.uwu@gmail.com>
2023-11-22 12:20:33 +0200

    lab 1 finished

---
+++ app/src/main/java/com/example/animalapp/SecondActivity.kt
@@ -0,0 +1,33 @@
+package com.example.animalapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.secondactivity)
+
+        val button2 = findViewById<Button>(R.id.buttonBack)
+        button2.setOnClickListener {
+            val intent2 = Intent(this, MainActivity::class.java)
+            startActivity(intent2)
+        }
+
+        val animalName = intent.getStringExtra("animalName")
+        val animalDescription = intent.getStringExtra("animalDescription")
+        val animalImageResId = intent.getIntExtra("animalImageResId", 1)
+
+        val animalImage = findViewById<ImageView>(R.id.image)
+        val animalNameView = findViewById<TextView>(R.id.name)
+        val animalDescriptionView = findViewById<TextView>(R.id.description)
+
+        animalImage.setImageResource(animalImageResId)
+        animalNameView.text = animalName
+        animalDescriptionView.text = animalDescription
+    }
+}
---
+++ app/src/main/java/com/example/animalapp/animal.kt
@@ -0,0 +1,8 @@
+package com.example.animalapp
+
+data class animal (val name : String, val description : String, val image: Int)
+
+val animal1 = animal("Cat", "The cat is a four-legged little predator that has conquered human hearts with its unmatched grace and mystique. Its round eyes always seem to hold the secrets of the universe, making it an unparalleled companion for millions of people.", R.drawable.animal1)
+val animal2 = animal("Dog", "The dog, a loyal and devoted companion, is a four-legged friend known for its unwavering loyalty and boundless enthusiasm. Whether bounding with joy at the sight of its owner or curling up for a nap, a dog's love is unconditional.", R.drawable.animal2)
+val animal3 = animal("Humster", "The hamster, a pint-sized bundle of fur, is a delightful and low-maintenance pet. With its small size and endearing round cheeks, a hamster is an ideal choice for those seeking a charming companion without the demands of larger pets.", R.drawable.animal3)
+
